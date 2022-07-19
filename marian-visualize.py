fb51bb3706b469d033767d557d48a851a7cf58ad
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2022-07-19 15:34:53 -0700

    Save state in a pickle file; read logs in a loop

--- marian-visualize.py
+++ marian-visualize.py
@@ -2,7 +2,8 @@
 
 import argparse
 import calendar
-import datetime
+import pickle
+import signal
 import sys
 import tensorboard as tb
 import tensorboardX as tbx
@@ -13,15 +14,49 @@ from pathlib import Path
 
 
 class LogFileReader(object):
-    def __init__(self, parser):
+    def __init__(self, path, parser, workdir):
+        self.log_file = Path(path)
         self.parser = parser
+        if workdir:
+            self.state_file = Path(workdir) / "state.pkl"
+        else:
+            self.state_file = None
+        self.last_update = None
+        self.last_line = None
 
-    def read(self, path):
-        with open(path, "r", encoding="utf-8") as logs:
-            for line in logs:
-                for output in self.parser.parse_line(line):
-                    sys.stderr.write(f"{output}\n")
-                    yield output
+        self._load_state()
+
+        sys.stderr.write(f"Log file {self.log_file} last updated at {self.last_update}\n")
+        sys.stderr.write(f"Previously processed lines: {self.last_line}\n")
+
+    def read(self):
+        if not self._need_update():
+            sys.stderr.write(f"No need to update {self.log_file}...\n")
+            return
+        with open(self.log_file, "r", encoding="utf-8") as logs:
+            for line_no, line in enumerate(logs):
+                if self.last_line and self.last_line < line_no:
+                    for output in self.parser.parse_line(line):
+                        sys.stderr.write(f"{output}\n")
+                        yield output
+                self.last_line = line_no
+                self.last_update = self.log_file.stat().st_mtime
+            self._save_state()
+
+    def _load_state(self):
+        if not Path(self.state_file).exists():
+            return
+        with open(self.state_file, "rb") as fstate:
+            self.last_update, self.last_line = pickle.load(fstate)
+
+    def _save_state(self):
+        with open(self.state_file, "wb") as fstate:
+            pickle.dump((self.last_update, self.last_line), fstate)
+
+    def _need_update(self):
+        if self.last_update and self.last_update <= self.log_file.stat().st_mtime:
+            return False
+        return True
 
 
 class MarianLogParser(object):
@@ -77,27 +112,43 @@ class AzureMLMetricsWriter(LogWriter):
 
 def main():
     args = parse_user_args()
+    
+    # signal.signal(signal.SIGTERM, service_shutdown)
+    # signal.signal(signal.SIGINT, service_shutdown)
 
-    reader = LogFileReader(parser=MarianLogParser())
+    threads = []
     for log_file in args.log_file:
-        log_dir = Path(args.tb_logdir) / Path(log_file).stem
-        writer = TensorboardWriter(log_dir)
-        for tup in reader.read(log_file):
-            writer.write(*tup)
+        thread = threading.Thread(target=setup_convertion, args=([log_file, args.tb_logdir]))
+        threads.append(thread)
+        thread.start()
 
     if args.offline:
+        for thread in threads:
+            thread.join()
         print("Done")
         exit()
 
     print(f"Starting TensorBoard...")
     launch_tensorboard(args)
-    # thread = threading.Thread(target=launch_tensorboard, args=([args]))
-    # thread.start()
 
-    # Do other stuff...
-
-    # thread.join()
+    for thread in threads:
+        thread.join()
     print("Done")
+
+
+# def service_shutdown(signum, frame):
+    # print('Caught signal %d' % signum)
+    # raise ServiceExit
+
+
+def setup_convertion(log_file, work_dir):
+    log_dir = Path(work_dir) / Path(log_file).stem
+    reader = LogFileReader(path=log_file, workdir=log_dir, parser=MarianLogParser())
+    writer = TensorboardWriter(log_dir)
+    while True:
+        for tup in reader.read():
+            writer.write(*tup)
+        time.sleep(5)
 
 
 def launch_tensorboard(args):
